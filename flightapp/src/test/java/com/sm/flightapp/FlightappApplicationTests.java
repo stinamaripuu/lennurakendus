b8674f0167d10de44146c6702d86a85f528c4533
Stina-Marie Maripuu <sinamaripuu@gmail.com>
2025-03-20 02:08:34 +0200

    init

---
+++ flightapp/src/test/java/com/sm/flightapp/FlightappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.flightapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
