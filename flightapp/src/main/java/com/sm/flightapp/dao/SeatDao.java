8d6adb9c8c2960fbce2b04bcb3bd76b380625c5b
Stina-Marie Maripuu <sinamaripuu@gmail.com>
2025-03-23 02:37:11 +0200

    istekoha valimine / muutmine backendis valmis

--- flightapp/src/main/java/com/sm/flightapp/dao/SeatDao.java
+++ flightapp/src/main/java/com/sm/flightapp/dao/SeatDao.java
@@ -3,6 +3,9 @@ package com.sm.flightapp.dao;
 import com.sm.flightapp.model.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SeatDao extends JpaRepository<Seat, Integer> {
 
+    List<Seat> findByFlightID(Integer flightId);
 }
